474975ba5854568a3e3c69fbe9ece5a360d0c0a1
parktaeyoul <xoduf1447@gmail.com>
2016-07-15 22:03:29 -0700

    add send command, send cicweb

--- broad.py
+++ broad.py
@@ -4,32 +4,41 @@ import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import os
 import MySQLdb.cursors
-token = 'your_token'
+token = 'insert token'
 
 def send_arbeit():
         for data in arbeit_data:
                 msg = '등록일 : ' + data[0].encode('utf-8') + '\n' + '회사명 : ' + data[1].encode('utf-8') + '\n' + '업무내용 : ' + data[2].encode('utf-8') + '\n' + '바로가기 : ' + data[3].encode('utf-8')
                 for u in user:
-                        updater.bot.sendMessage(chat_id=u[0].encode('utf-8'), text=msg)
+			if u[1]==1:
+                        	updater.bot.sendMessage(chat_id=u[0].encode('utf-8'), text=msg)
 
 def send_room():
         for data in room_data:
                 msg = '등록일 : ' + data[0].encode('utf-8') + '\n' + '위치 : ' + data[1].encode('utf-8') + '\n' + '제목 : ' + data[2].encode('utf-8') + '\n' + '가격대(만원) : ' + data[3].encode('utf-8') + '\n' + '바로가기 : ' + data[4].encode('utf-8')
                 for u in user:
-                        updater.bot.sendMessage(chat_id=u[0].encode('utf-8'), text=msg)
+			if u[2]==1:
+                        	updater.bot.sendMessage(chat_id=u[0].encode('utf-8'), text=msg)
 
 def send_barter():
         for data in barter_data:
                 msg = '등록일 : ' + data[0].encode('utf-8') + '\n' + '제목 : ' + data[1].encode('utf-8') + '\n' + '글쓴이 : ' + data[2].encode('utf-8') + '\n' + '바로가기 : ' + data[3].encode('utf-8')
                 for u in user:
-                        updater.bot.sendMessage(chat_id=u[0].encode('utf-8'), text=msg)
+			if u[3]==1:
+                        	updater.bot.sendMessage(chat_id=u[0].encode('utf-8'), text=msg)
 
+def send_cicweb():
+	for data in cicweb_data:
+		msg = '등록일 : ' +  data[0].encode('utf-8') + '\n' + '제목: ' + data[1].encode('utf-8') + '\n' + '글쓴이 : ' + data[2].encode('utf-8') + '\n' + '바로가기 : ' + data[3].encode('utf-8')
+		for u in user:
+			if u[4]==1:
+				updater.bot.sendMessage(chat_id=u[0].encode('utf-8'), text=msg)
 def broad():
 	try:
-		conn = MySQLdb.connect(user='user', passwd='passwd', db='uou_alarmi', host='localhost', charset='utf8', use_unicode='True')
+		conn = MySQLdb.connect(user='alarmi', passwd='alarmi', db='uou_alarmi', host='localhost', charset='utf8', use_unicode='True')
 		cursor = conn.cursor()
 
-		cursor.execute("select userid from uou_alarmi_userdb")
+		cursor.execute("select userid, arbeit, room, barter, cicweb from uou_alarmi_userdb")
 		global user
 		user = cursor.fetchall()
 		
@@ -44,7 +53,11 @@ def broad():
 		cursor.execute("select * from uou_alarmi_barter")
 		global barter_data
 		barter_data = cursor.fetchall()
-	
+		
+		cursor.execute("select * from uou_alarmi_cicweb")
+		global cicweb_data
+		cicweb_data = cursor.fetchall()
+
 	except MySQLdb.Error, e:
 		print "Error %d: %s" % (e.args[0], e.args[1])
 		sys.exit(1)
@@ -57,7 +70,9 @@ def broad():
 
 	if barter_data != None:
 		send_barter()
-
+	
+	if cicweb_data != None:
+		send_cicweb()
 
 updater = Updater(token)
 
--- uou_alarmi_bot.py
+++ uou_alarmi_bot.py
@@ -8,11 +8,11 @@ import MySQLdb.cursors
 
 urllib3.disable_warnings()
 
-token = 'your_token'
+token = 'insert token'
 
 def start(bot, update):
 	try:
-		conn = MySQLdb.connect(user='user', passwd='passwd', db='uou_alarmi', host='localhost', charset='utf8', use_unicode='True')
+		conn = MySQLdb.connect(user='alarmi', passwd='alarmi', db='uou_alarmi', host='localhost', charset='utf8', use_unicode='True')
 		cursor = conn.cursor()
 	
 	except MySQLdb.Error, e:
@@ -37,7 +37,7 @@ def start(bot, update):
 		try:
 			cursor.execute("""insert into uou_alarmi.uou_alarmi_userdb (userid) values (%s)""" % str(user_id))
 			conn.commit()
-			msg = '가입 완료. 울산대 알리미가 알림을 시작합니다.'
+			msg = '가입 완료. 울산대 알리미가 알림을 시작합니다.\n\n----UOU알라미 Command----\n시작하기 : /start\n중지하기 : /stop\n도움말 : /help\n문수장터 알림 : /sale\n문수알바 알림 : /arbeit\n문수복덕방 알림 : /room\ncicweb 공지 : /cic'
 		
 		except MySQLdb.Error, e:
 			print "Error %d: %s" % (e.args[0], e.args[1])
@@ -48,7 +48,7 @@ def start(bot, update):
 	
 def stop(bot, update):
 	try:
-		conn = MySQLdb.connect(user='molkoo', passwd='7qmffor', db='uou_alarmi', host='localhost', charset='utf8', use_unicode='True')
+		conn = MySQLdb.connect(user='alarmi', passwd='alarmi', db='uou_alarmi', host='localhost', charset='utf8', use_unicode='True')
 		cursor = conn.cursor()
 		
 	except MySQLdb.Error, e:
@@ -81,13 +81,201 @@ def stop(bot, update):
 	conn.close()
 	bot.sendMessage(chat_id=user_id, text=msg)
 
+def help(bot, update):
+	user_id = update.message.chat_id
+	msg = '----UOU알라미 Command----\n시작하기 : /start\n중지하기 : /stop\n도움말 : /help\n문수장터 알림 : /sale\n문수알바 알림 : /arbeit\n문수복덕방 알림 : /room\ncicweb 공지 : /cic'
+	bot.sendMessage(chat_id=user_id, text=msg)
+
+def arbeit(bot, update):
+	try:
+		conn = MySQLdb.connect(user='alarmi', passwd='alarmi', db='uou_alarmi', host='localhost', charset='utf8', use_unicode='True')
+		cursor = conn.cursor()
+	
+	except MySQLdb.Error, e:
+		print "Error %d: %s" % (e.args[0], e.args[1])
+		sys.exit(1)
+
+
+	user_id = update.message.chat_id
+
+	try:
+		cursor.execute("""select arbeit from uou_alarmi_userdb where userid=%s""" % str(user_id))
+		result = cursor.fetchone()
+	
+	except MySQLdb.Error, e:
+		print "Error %d: %s" % (e.args[0], e.args[1])
+		sys.exit(1)
+	
+	if result[0]==1:
+		try:
+			cursor.execute("""update uou_alarmi_userdb set arbeit = 0 where userid=%s""" % str(user_id))
+			conn.commit()
+			msg = '문수알바 알림을 해제합니다.'
+		
+		except MySQLdb.Error, e:
+			print "Error %d: %s" % (e.args[0], e.args[1])
+			sys.exit(1)
+
+	else:
+		try:
+			cursor.execute("""update uou_alarmi_userdb set arbeit = 1 where userid=%s""" % str(user_id))
+			conn.commit()
+			msg = 'UOU알라미가 최신 문수알바 내용을 알려줍니다.'
+		
+		except MySQLdb.Error, e:
+			print "Error %d: %s" % (e.args[0], e.args[1])
+			sys.exit(1)
+
+	conn.close()
+	bot.sendMessage(chat_id=user_id, text=msg)
+
+def room(bot, update):
+	try:
+		conn = MySQLdb.connect(user='alarmi', passwd='alarmi', db='uou_alarmi', host='localhost', charset='utf8', use_unicode='True')
+		cursor = conn.cursor()
+	
+	except MySQLdb.Error, e:
+		print "Error %d: %s" % (e.args[0], e.args[1])
+		sys.exit(1)
+
+
+	user_id = update.message.chat_id
+
+	try:
+		cursor.execute("""select room from uou_alarmi_userdb where userid=%s""" % str(user_id))
+		result = cursor.fetchone()
+	
+	except MySQLdb.Error, e:
+		print "Error %d: %s" % (e.args[0], e.args[1])
+		sys.exit(1)
+	
+	if result[0]==1:
+		try:
+			cursor.execute("""update uou_alarmi_userdb set room = 0 where userid=%s""" % str(user_id))
+			conn.commit()
+			msg = '문수복덕방 알림을 해제합니다.'
+		
+		except MySQLdb.Error, e:
+			print "Error %d: %s" % (e.args[0], e.args[1])
+			sys.exit(1)
+
+	else:
+		try:
+			cursor.execute("""update uou_alarmi_userdb set room = 1 where userid=%s""" % str(user_id))
+			conn.commit()
+			msg = 'UOU알라미가 최신 문수복덕방 내용을 알려줍니다.'
+		
+		except MySQLdb.Error, e:
+			print "Error %d: %s" % (e.args[0], e.args[1])
+			sys.exit(1)
+
+	conn.close()
+	bot.sendMessage(chat_id=user_id, text=msg)
+
+def barter(bot, update):
+	try:
+		conn = MySQLdb.connect(user='alarmi', passwd='alarmi', db='uou_alarmi', host='localhost', charset='utf8', use_unicode='True')
+		cursor = conn.cursor()
+	
+	except MySQLdb.Error, e:
+		print "Error %d: %s" % (e.args[0], e.args[1])
+		sys.exit(1)
+
+
+	user_id = update.message.chat_id
+
+	try:
+		cursor.execute("""select barter from uou_alarmi_userdb where userid=%s""" % str(user_id))
+		result = cursor.fetchone()
+	
+	except MySQLdb.Error, e:
+		print "Error %d: %s" % (e.args[0], e.args[1])
+		sys.exit(1)
+	
+	if result[0]==1:
+		try:
+			cursor.execute("""update uou_alarmi_userdb set barter = 0 where userid=%s""" % str(user_id))
+			conn.commit()
+			msg = '문수장터 알림을 해제합니다.'
+		
+		except MySQLdb.Error, e:
+			print "Error %d: %s" % (e.args[0], e.args[1])
+			sys.exit(1)
+
+	else:
+		try:
+			cursor.execute("""update uou_alarmi_userdb set barter = 1 where userid=%s""" % str(user_id))
+			conn.commit()
+			msg = 'UOU알라미가 최신 문수장터 내용을 알려줍니다.'
+		
+		except MySQLdb.Error, e:
+			print "Error %d: %s" % (e.args[0], e.args[1])
+			sys.exit(1)
+
+	conn.close()
+	bot.sendMessage(chat_id=user_id, text=msg)
+
+def cicweb(bot, update):
+	try:
+		conn = MySQLdb.connect(user='alarmi', passwd='alarmi', db='uou_alarmi', host='localhost', charset='utf8', use_unicode='True')
+		cursor = conn.cursor()
+	
+	except MySQLdb.Error, e:
+		print "Error %d: %s" % (e.args[0], e.args[1])
+		sys.exit(1)
+
+
+	user_id = update.message.chat_id
+
+	try:
+		cursor.execute("""select cicweb from uou_alarmi_userdb where userid=%s""" % str(user_id))
+		result = cursor.fetchone()
+	
+	except MySQLdb.Error, e:
+		print "Error %d: %s" % (e.args[0], e.args[1])
+		sys.exit(1)
+	
+	if result[0]==1:
+		try:
+			cursor.execute("""update uou_alarmi_userdb set cicweb = 0 where userid=%s""" % str(user_id))
+			conn.commit()
+			msg = 'cicweb 공지사항 알림을 해제합니다.'
+		
+		except MySQLdb.Error, e:
+			print "Error %d: %s" % (e.args[0], e.args[1])
+			sys.exit(1)
+
+	else:
+		try:
+			cursor.execute("""update uou_alarmi_userdb set cicweb = 1 where userid=%s""" % str(user_id))
+			conn.commit()
+			msg = 'UOU알라미가 최신 cicweb 공지사항 내용을 알려줍니다.'
+		
+		except MySQLdb.Error, e:
+			print "Error %d: %s" % (e.args[0], e.args[1])
+			sys.exit(1)
+
+	conn.close()
+	bot.sendMessage(chat_id=user_id, text=msg)
+
 updater = Updater(token)
 dispatcher = updater.dispatcher
 
 start_handler = CommandHandler('start', start)
 stop_handler = CommandHandler('stop', stop)
+help_handler = CommandHandler('help', help)
+arbeit_handler = CommandHandler('arbeit', arbeit)
+room_handler = CommandHandler('room',room)
+barter_handler = CommandHandler('sale',barter)
+cicweb_handler = CommandHandler('cic',cicweb)
 
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(stop_handler)
+dispatcher.add_handler(help_handler)
+dispatcher.add_handler(arbeit_handler)
+dispatcher.add_handler(room_handler)
+dispatcher.add_handler(barter_handler)
+dispatcher.add_handler(cicweb_handler)
+
 
 updater.start_polling()
